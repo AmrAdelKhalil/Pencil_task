47a2a5de42390b692fee6ed2dda4ca81985387fc
Amr Adel <amradelkhalil@gmail.com>
2023-07-03 14:55:43 +0200

    Fix db url

--- app/index.js
+++ app/index.js
@@ -4,9 +4,14 @@ if (process.env.NODE_ENV === 'test') {
     mongoose.connect(
         "mongodb://admin:admin@localhost:27017/pencil?authSource=admin"
     );
-} else {
+} else if(process.env.NODE_ENV === 'dev') {
     mongoose.connect(
         `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}?authSource=admin`
+    );
+}
+else {
+    mongoose.connect(
+        "mongodb+srv://${process.env.DATABASE_USERNAME:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}?retryWrites=true&w=majority"
     );
 }
 
